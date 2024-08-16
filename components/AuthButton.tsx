6474ae7db6c93828db75c644486a68761987c2ff
우혜민 <woo28870826@gmail.com>
2024-08-16 20:00:56 +0900

    styles: 로그인 버튼 공통 컴포넌트로 교체

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
-import Link from "next/link";
 import { redirect } from "next/navigation";
+import Button from "./Button";
 
 export default async function AuthButton() {
   const supabase = createClient();
@@ -27,11 +27,8 @@ export default async function AuthButton() {
       </form>
     </div>
   ) : (
-    <Link
-      href="/login"
-      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
-    >
+    <Button href="/login" variant="outlined">
       로그인
-    </Link>
+    </Button>
   );
 }
