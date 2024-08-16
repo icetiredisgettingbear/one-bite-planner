46834ffc41d6e128ca33628df1ec32cef6edf689
우혜민 <woo28870826@gmail.com>
2024-08-16 17:49:07 +0900

    styles: Supabase, Next.js에서 기본으로 제공하는 UI 수정 및 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { GeistSans } from "geist/font/sans";
+import { createClient } from "@/utils/supabase/server";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -16,10 +18,24 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const canInitSupabaseClient = () => {
+    // This function is just for the interactive tutorial.
+    // Feel free to remove it once you have Supabase connected.
+    try {
+      createClient();
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  const isSupabaseConnected = canInitSupabaseClient();
+
   return (
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
+          <Header isSupabaseConnected={isSupabaseConnected} />
           {children}
         </main>
       </body>
--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -19,10 +19,10 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      안녕하세요, {user.email}!
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-          Logout
+          로그아웃
         </button>
       </form>
     </div>
@@ -31,7 +31,7 @@ export default async function AuthButton() {
       href="/login"
       className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
     >
-      Login
+      로그인
     </Link>
   );
 }
