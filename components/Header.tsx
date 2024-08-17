607e9c28df0354142d0c7239076fe3a820db6342
우혜민 <woo28870826@gmail.com>
2024-08-17 16:42:23 +0900

    styles: 헤더 디자인 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,7 @@
 import AuthButton from "@/components/AuthButton";
+import Link from "next/link";
+import Typography from "./Typography";
+import Image from "next/image";
 
 interface HeaderProps {
   isSupabaseConnected: boolean;
@@ -6,21 +9,37 @@ interface HeaderProps {
 
 export default function Header({ isSupabaseConnected }: HeaderProps) {
   return (
-    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-      <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-        <div className="flex gap-x-12 items-center">
-          <h1 className="text-xl font-bold">한입 플래너</h1>
-          <ul className="flex space-x-6">
-            <li className="text-base text-gray-800 font-medium">목표설정</li>
-            <li className="text-base text-gray-800 font-medium">실행하기</li>
-            <li className="text-base text-gray-800 font-medium">돌아보기</li>
+    <nav className="w-full flex justify-center bg-white border-solid border-b border-b-foreground/10 h-20">
+      <div className="w-full max-w-[1280px] px-10 flex justify-between items-center">
+        <div className="flex gap-x-8 items-center">
+          <Link className="flex justify-center" href="/">
+            <Image
+              src="/images/one_bite_planner_logo.svg"
+              alt="한입 플래너 로고"
+              width={120}
+              height={28}
+            />
+          </Link>
+          <ul className="flex gap-x-12 list-none none">
+            <li className="text-base text-gray-800 font-medium">
+              <Link className="no-underline text-[#333]" href="/goals-setup">
+                <Typography variant="subtitle2">목표설정</Typography>
+              </Link>
+            </li>
+            <li className="text-base text-gray-800 font-medium">
+              <Link className="no-underline text-[#333]" href="/to-do">
+                <Typography variant="subtitle2">실행하기</Typography>
+              </Link>
+            </li>
+            <li className="text-base text-gray-800 font-medium">
+              <Link className="no-underline text-[#333]" href="/progress">
+                <Typography variant="subtitle2">돌아보기</Typography>
+              </Link>
+            </li>
           </ul>
         </div>
 
-        <div className="flex gap-x-2 items-center">
-          {/* <DeployButton /> */}
-          {isSupabaseConnected && <AuthButton />}
-        </div>
+        {isSupabaseConnected && <AuthButton />}
       </div>
     </nav>
   );
