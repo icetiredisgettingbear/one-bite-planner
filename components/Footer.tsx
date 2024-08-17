6e9e56dc40195a19d6264c15d86f437d7d8ac04b
우혜민 <woo28870826@gmail.com>
2024-08-17 16:39:58 +0900

    styles: 푸터 MUI 컴포넌트 적용 및 디자인 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
+import Typography from "./Typography";
+
 export default function Footer() {
   return (
-    <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-      <p>ⓒ 한입 플래너. All rights reserved.</p>
+    <footer className="w-full max-w-[1280px] px-10 py-8 flex text-center">
+      <Typography variant="body2" className="text-disabled">
+        ⓒ 한입 플래너. All rights reserved.
+      </Typography>
     </footer>
   );
 }
