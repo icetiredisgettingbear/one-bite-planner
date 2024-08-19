91f0fcba5417bb8985a56b06d01d1319d8092672
우혜민 <woo28870826@gmail.com>
2024-08-19 22:52:49 +0900

    styles: Header에 공통 컴포넌트 적용하여 개선

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,17 @@
 import AuthButton from "@/components/AuthButton";
 import Link from "next/link";
-import Typography from "./Typography";
 import Image from "next/image";
+import Menus, { Menu } from "./Menus";
+import AppBar from "@mui/material/AppBar";
+import Container from "./Container";
+import Toolbar from "./Toolbar";
+import Stack from "./Stack";
+
+const menus: Menu[] = [
+  { pathname: "/goals-setup", label: "계획하기" },
+  { pathname: "/to-do", label: "실행하기" },
+  { pathname: "/progress", label: "돌아보기" },
+];
 
 interface HeaderProps {
   isSupabaseConnected: boolean;
@@ -9,38 +19,27 @@ interface HeaderProps {
 
 export default function Header({ isSupabaseConnected }: HeaderProps) {
   return (
-    <nav className="w-full flex justify-center bg-white border-solid border-b border-b-foreground/10 h-20">
-      <div className="w-full max-w-[1280px] px-10 flex justify-between items-center">
-        <div className="flex gap-x-8 items-center">
-          <Link className="flex justify-center" href="/">
-            <Image
-              src="/images/one_bite_planner_logo.svg"
-              alt="한입 플래너 로고"
-              width={120}
-              height={28}
-            />
-          </Link>
-          <ul className="flex gap-x-12 list-none none">
-            <li className="text-base text-gray-800 font-medium">
-              <Link className="no-underline text-[#333]" href="/goals-setup">
-                <Typography variant="subtitle2">목표설정</Typography>
-              </Link>
-            </li>
-            <li className="text-base text-gray-800 font-medium">
-              <Link className="no-underline text-[#333]" href="/to-do">
-                <Typography variant="subtitle2">실행하기</Typography>
-              </Link>
-            </li>
-            <li className="text-base text-gray-800 font-medium">
-              <Link className="no-underline text-[#333]" href="/progress">
-                <Typography variant="subtitle2">돌아보기</Typography>
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        {isSupabaseConnected && <AuthButton />}
-      </div>
-    </nav>
+    <AppBar position="fixed" color="inherit" elevation={1}>
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Stack direction="row" flexGrow={1} gap={6}>
+            <Link
+              href="/"
+              passHref
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <Image
+                src="/images/one_bite_planner_logo.svg"
+                alt="한입 플래너 로고"
+                width={100}
+                height={22}
+              />
+            </Link>
+            <Menus menus={menus} />
+          </Stack>
+          {isSupabaseConnected && <AuthButton />}
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
