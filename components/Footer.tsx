2307f41798b6edd43e7c3e372036e8c6be7f5c1e
우혜민 <woo28870826@gmail.com>
2024-08-19 16:54:05 +0900

    styles: Footer 컴포넌트 공통 컴포넌트로 적용

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,12 @@
+import Container from "./Container";
 import Typography from "./Typography";
 
 export default function Footer() {
   return (
-    <footer className="w-full max-w-[1280px] px-10 py-8 flex text-center">
-      <Typography variant="body2" className="text-disabled">
+    <Container component="footer" maxWidth="lg" sx={{ py: 5 }}>
+      <Typography variant="body2" color="disabled">
         ⓒ 한입 플래너. All rights reserved.
       </Typography>
-    </footer>
+    </Container>
   );
 }
