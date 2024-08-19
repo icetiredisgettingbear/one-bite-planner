084417583900f5a9173da38f0ae90b8c6b4e4a66
우혜민 <woo28870826@gmail.com>
2024-08-19 22:37:24 +0900

    styles: AuthButton에 공통 컴포넌트 적용

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -2,6 +2,7 @@ import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import Button from "./Button";
 import Typography from "./Typography";
+import Stack from "./Stack";
 
 export default async function AuthButton() {
   const supabase = createClient();
@@ -19,14 +20,14 @@ export default async function AuthButton() {
   };
 
   return user ? (
-    <div className="flex items-center gap-4">
+    <Stack direction="row" alignItems="center" gap={2}>
       <Typography variant="body2">안녕하세요, {user.email}!</Typography>
       <form action={signOut}>
         <Button variant="contained" color="info" size="small" type="submit">
           로그아웃
         </Button>
       </form>
-    </div>
+    </Stack>
   ) : (
     <Button href="/login" size="small">
       로그인
