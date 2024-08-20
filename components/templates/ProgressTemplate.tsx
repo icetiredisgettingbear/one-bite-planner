59b52427c370af0d7ab23d769a650f2ffc97edcc
우혜민 <woo28870826@gmail.com>
2024-08-21 04:36:17 +0900

    styles: ProgressTemplate 반응형 작업

--- components/templates/ProgressTemplate.tsx
+++ components/templates/ProgressTemplate.tsx
@@ -3,11 +3,16 @@
 import Typography from "@/components/Typography";
 import Box from "../Box";
 import TemplateLayout from "../layouts/TemplateLayout";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function ProgressTemplate() {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <TemplateLayout>
-      <Typography variant="h2">
+      <Typography variant={isSmallScreen ? "h4" : "h2"}>
         목표를 향해 얼마큼 해냈는지 확인해 보세요.
       </Typography>
       <Box
