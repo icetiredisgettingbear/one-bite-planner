30af6689e252e22ae645f465dba1ce1a3796b7d0
우혜민 <woo28870826@gmail.com>
2024-08-20 15:41:53 +0900

    feat: 분기별 목표 설정 화면 작업

--- components/templates/QuarterGoalTemplate.tsx
+++ components/templates/QuarterGoalTemplate.tsx
@@ -5,85 +5,83 @@ import TextField from "@/components/TextField";
 import Typography from "@/components/Typography";
 import { createClient } from "@/utils/supabase/client";
 import { useRouter } from "next/navigation";
-import { FormEvent, ChangeEvent, useState } from "react";
+import { ChangeEvent, useState, FormEventHandler } from "react";
+import TemplateLayout from "../layouts/TemplateLayout";
+import Stack from "../Stack";
+import { getCurrentDateInfo } from "@/utils/dateUtils";
+
+type FormData = {
+  quarterlyGoal1: string;
+  quarterlyGoal2: string;
+  quarterlyGoal3: string;
+};
+
+const goals: (keyof FormData)[] = [
+  "quarterlyGoal1",
+  "quarterlyGoal2",
+  "quarterlyGoal3",
+];
 
 export default function QuarterGoalTemplate() {
   const supabase = createClient();
   const router = useRouter();
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormData>({
     quarterlyGoal1: "",
     quarterlyGoal2: "",
     quarterlyGoal3: "",
   });
+  const { year, quarter } = getCurrentDateInfo();
+
+  const commonInsertData = { is_achieved: false, year, quarter };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit: FormEventHandler<HTMLDivElement> = async (e) => {
     e.preventDefault();
 
-    const { error } = await supabase.from("quarterly_goals").insert([
-      {
-        is_achieved: false,
-        year: 2024,
-        quarter: 3,
-        goal: formData.quarterlyGoal1,
-      },
-      {
-        is_achieved: false,
-        year: 2024,
-        quarter: 3,
-        goal: formData.quarterlyGoal2,
-      },
-      {
-        is_achieved: false,
-        year: 2024,
-        quarter: 3,
-        goal: formData.quarterlyGoal3,
-      },
-    ]);
+    const goalsToInsert = goals.map((goal) => ({
+      ...commonInsertData,
+      goal: formData[goal],
+    }));
+
+    const { error } = await supabase
+      .from("quarterly_goals")
+      .insert(goalsToInsert);
+
+    if (error) {
+      console.error("Error inserting quarterly goals:", error.message);
+      return;
+    }
 
     router.push("/goals-setup/month");
   };
 
   return (
-    <div className="flex-1 w-full max-w-[1280px] px-10 flex flex-col gap-5 justify-center">
-      <div className="flex flex-col gap-3">
-        <Typography variant="h2">이번 분기에 무엇을 해야할까요?</Typography>
-        <Typography variant="subtitle2" className="text-secondary">
+    <TemplateLayout>
+      <Stack gap={2}>
+        <Typography variant="h2">
+          이번 {quarter}분기에 무엇을 해야 할까요?
+        </Typography>
+        <Typography variant="subtitle2" color="secondary">
           최대 3개까지 입력할 수 있어요.
         </Typography>
-      </div>
-      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-        <TextField
-          name="quarterlyGoal1"
-          label="분기 목표 1"
-          placeholder="이번 분기 목표를 알려주세요"
-          size="large"
-          fullWidth
-          onChange={handleChange}
-        />
-        <TextField
-          name="quarterlyGoal2"
-          label="분기 목표 2"
-          placeholder="이번 분기 목표를 알려주세요"
-          size="large"
-          fullWidth
-          onChange={handleChange}
-        />
-        <TextField
-          name="quarterlyGoal3"
-          label="분기 목표 3"
-          placeholder="이번 분기 목표를 알려주세요"
-          size="large"
-          fullWidth
-          onChange={handleChange}
-        />
-        <div className="flex gap-3 mt-10">
+      </Stack>
+      <Stack component="form" gap={2} onSubmit={handleSubmit}>
+        {goals.map((goal, index) => (
+          <TextField
+            key={goal}
+            name={goal}
+            label={`분기 목표 ${index + 1}`}
+            placeholder="이번 분기 목표를 알려주세요"
+            size="large"
+            fullWidth
+            onChange={handleChange}
+          />
+        ))}
+        <Stack direction="row" gap={1.5} mt={6}>
           <Button
             variant="contained"
             size="medium"
@@ -95,8 +93,8 @@ export default function QuarterGoalTemplate() {
           <Button variant="contained" size="medium" type="submit">
             다음
           </Button>
-        </div>
-      </form>
-    </div>
+        </Stack>
+      </Stack>
+    </TemplateLayout>
   );
 }
